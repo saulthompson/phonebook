4f931231bacc84583583e6778e35e06d3129f432
Saul <saulthompson@hotmail.co.uk>
2024-07-14 19:00:14 +0100

    added npm start script

--- backend/index.js
+++ backend/index.js
@@ -99,6 +99,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`server running on ${PORT}`);
