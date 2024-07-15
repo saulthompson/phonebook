dc57a16697594a1583ac3504b03a75531dfdf823
Saul <saulthompson@hotmail.co.uk>
2024-07-15 15:29:47 +0100

    fixed bug

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Contact = require('./models/contact')
 
+const app = express()
+
 app.use(express.static('dist'))
 
-const app = express()
 
 app.use(express.json())
 app.use(cors())
